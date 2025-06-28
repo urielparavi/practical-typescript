7e2d1ddfd50c765654f2865334becb5f18b40516
Your Name <urielparavi75@gmail.com>
2025-06-28 05:27:39 +0300

    13. Type : Any

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,14 +1,5 @@
-let tax: number | string = 10;
-tax = 100;
-tax = '$10';
+let notSure: any = 4;
+notSure = 'maybe a string instead';
+notSure = false;
 
-// This variable uses a union of string literal types, meaning it can only hold one of the three specific
-// values: 'pending', 'success', or 'error'.
-// This is useful for modeling limited state options, such as the status of a network request, and provides
-// strong type safety.
-// It prevents assigning invalid strings, improves code readability, and enables better autocomplete and error
-// checking in editors like VS Code.
-let requestStatus: 'pending' | 'success' | 'error' = 'pending';
-requestStatus = 'success';
-requestStatus = 'error';
-// requestStatus = 'random';
+let random;
