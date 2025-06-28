e4de5a0d50db21ad8d2f543cd6386a8310ad814b
Your Name <urielparavi75@gmail.com>
2025-06-28 15:54:28 +0300

    17. Challenge - Arrays

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,12 +1,7 @@
-let prices: number[] = [100, 75, 42];
-// prices.push('hello');
+let temperatures: number[] = [20, 25, 30];
+// temperatures.push('hot');
 
-let fruit: string[] = ['apple', 'orange'];
+let colors: string[] = ['red', 'green', 'blue'];
+// colors.push(true);
 
-// let randomValues: [] = ['hello'];
-
-let emptyValues: number[] = [];
-
-let names = ['peter', 'susan', 1];
-
-let array: (string | boolean)[] = ['apple', true, 'orange', false];
+let mixedArray: (number | string)[] = [1, 'two', 3];
