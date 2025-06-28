f07d68b5cb8c7856385934da30107fc572cb2a98
Your Name <urielparavi75@gmail.com>
2025-06-28 23:58:10 +0300

    29. Functions - Objects As Parameters

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,10 +1,24 @@
-function processInput(input: string | number) {
-  if (typeof input === 'number') {
-    console.log(input * 2);
-  } else {
-    console.log(input.toLocaleLowerCase());
-  }
+function createEmployee({ id }: { id: number }): {
+  id: number;
+  isActive: boolean;
+} {
+  return { id, isActive: id % 2 === 0 };
 }
 
-processInput(10);
-processInput('Hello');
+const first = createEmployee({ id: 1 }); // { id: 1, isActive: false }
+const second = createEmployee({ id: 2 }); // { id: 2, isActive: true }
+
+console.log(first, second); // { id: 1, isActive: false } { id: 2, isActive: true }
+
+// alternative
+
+function createStudent(student: { id: number; name: string }): void {
+  console.log(`Welcome to this example ${student.name.toUpperCase()}!!!`);
+}
+
+const newStudent = {
+  id: 5,
+  name: 'anna',
+};
+
+createStudent(newStudent); // Welcome to this example ANNA!!!
