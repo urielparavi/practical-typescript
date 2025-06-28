28eb1a5f1f8be0beb2b25dae9f8943acd90a6793
Your Name <urielparavi75@gmail.com>
2025-06-28 05:18:17 +0300

    12. Union Type

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,15 +1,14 @@
-let greeting: string = 'Hello, Typescript!';
-greeting = greeting.toUpperCase();
+let tax: number | string = 10;
+tax = 100;
+tax = '$10';
 
-let age: number = 25;
-age = age + 5;
-
-let isAdult: boolean = age >= 18;
-isAdult = !isAdult;
-console.log(isAdult);
-
-// greeting = 10;
-// age = 'thirty';
-// isAdult = 'yes';
-
-console.log(greeting, age, isAdult);
+// This variable uses a union of string literal types, meaning it can only hold one of the three specific
+// values: 'pending', 'success', or 'error'.
+// This is useful for modeling limited state options, such as the status of a network request, and provides
+// strong type safety.
+// It prevents assigning invalid strings, improves code readability, and enables better autocomplete and error
+// checking in editors like VS Code.
+let requestStatus: 'pending' | 'success' | 'error' = 'pending';
+requestStatus = 'success';
+requestStatus = 'error';
+// requestStatus = 'random';
