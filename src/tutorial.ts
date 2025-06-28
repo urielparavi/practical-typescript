cdd7f3c3a371ad532a772f66b494141675c4e3ab
Your Name <urielparavi75@gmail.com>
2025-06-28 20:06:46 +0300

    26. Functions - Rest Parameter

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,15 +1,14 @@
-function calculatePrice(price: number, discount?: number): number {
-  return price - (discount || 0);
+function sum(message: string, ...numbers: number[]): string {
+  const doubled = numbers.map((num) => num * 2);
+  console.log(doubled);
+
+  let total = numbers.reduce((previous, current) => {
+    return previous + current;
+  }, 0);
+
+  return `${message}${total}`;
 }
 
-let priceAfterDiscount = calculatePrice(100, 20);
+let result = sum('The total is: ', 1, 2, 3, 4, 5);
 
-function calculateScore(
-  initialScore: number,
-  penaltyPoints: number = 0
-): number {
-  return initialScore - penaltyPoints;
-}
-
-let scoreAfterPenalty = calculateScore(100, 2);
-let scoreWithoutPenalty = calculateScore(300);
+console.log(result);
