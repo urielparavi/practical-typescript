fd8c0d75b34c18b4fdd6b4295c081ec77ddaf932
Your Name <urielparavi75@gmail.com>
2025-06-28 04:43:04 +0300

    8. Type Annotations

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,1 +1,14 @@
-console.log('typescript tutorial');
+let awesomeName: string = 'shakeAndBake';
+awesomeName = 'something';
+awesomeName = awesomeName.toUpperCase();
+console.log(awesomeName);
+
+// awesomeName = 20;
+
+let amount: number = 20;
+amount = 12 - 1;
+// amount = 'pants';
+
+let isAwesome: boolean = true;
+isAwesome = false;
+// isAwesome = 'shakeAndBake';
