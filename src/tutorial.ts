f17adbd75fc41f5124b69c75d70f722ae8a16b56
Your Name <urielparavi75@gmail.com>
2025-06-28 18:21:15 +0300

    23. Challenge - Functions 1

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,9 +1,13 @@
-function addThree(number: any) {
-  let anotherNumber: number = 3;
-  return number + anotherNumber;
+const names: string[] = ['john', 'jane', 'jim', 'jill'];
+
+function isNameInList(name: string): boolean {
+  return names.includes(name);
 }
 
-const result = addThree(3);
-const someValue = result;
+let nameToCheck = 'peter';
 
-someValue.myMethod(); // type: any
+if (isNameInList(nameToCheck)) {
+  console.log(`${nameToCheck} is in the list`);
+} else {
+  console.log(`${nameToCheck} is NOT in the list`);
+}
