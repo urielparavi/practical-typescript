72a610ee2584cb9113f051b604bc584fe89055a1
Your Name <urielparavi75@gmail.com>
2025-06-29 18:30:13 +0300

    38. Interface - Methods

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -3,13 +3,24 @@ interface Book {
   title: string;
   author: string;
   genre?: string;
+  // method
+  printAuthor(): void;
+  printTitle(message: string): string;
 }
 
 const deepWork: Book = {
   isbn: 123,
   title: 'deep work',
   author: 'cal newport',
-  // genre: 'self-help',
+  genre: 'self-help',
+  printAuthor() {
+    console.log(this.author);
+  },
+  printTitle(message) {
+    return `${this.title} ${message}`;
+  },
 };
 
-// deepWork.isbn = 'some value';
+deepWork.printAuthor();
+const result = deepWork.printTitle('is awesome book');
+console.log(result);
