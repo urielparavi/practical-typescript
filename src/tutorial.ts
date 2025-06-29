795823a83a4bccd8f16b977602a0e8b2f5f4f0db
Your Name <urielparavi75@gmail.com>
2025-06-30 00:56:40 +0300

    44. Interface vs Type Alias

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -39,6 +39,20 @@ interface Manager extends Person {
   delegateTasks(): void;
 }
 
+// Get a random employee which can be a Person, DogOwner, or Manager
 const employee: Person | DogOwner | Manager = getEmployee();
 
-console.log(employee);
+// Type guard function:
+// Checks if the given object has the 'managePeople' method,
+// which only exists on the Manager type.
+// If so, TypeScript will treat the object as a Manager inside the if-block.
+function isManager(obj: Person | DogOwner | Manager): obj is Manager {
+  return 'managePeople' in obj;
+}
+
+// Check if the employee is a Manager.
+// Thanks to the type guard, TypeScript now knows that 'employee' is a Manager,
+// so we can safely call 'delegateTasks' without type errors.
+if (isManager(employee)) {
+  employee.delegateTasks();
+}
