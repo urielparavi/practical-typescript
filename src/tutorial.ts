ceb5eea81e86bea54c7931de1e6f04850cf278ab
Your Name <urielparavi75@gmail.com>
2025-06-30 03:48:01 +0300

    48. Challenge - Tuple and Enum

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,28 +1,28 @@
-enum ServerResponseStatus {
-  Success = 200,
-  Error = 500,
+// Define user roles using an enum for better readability and to avoid magic strings.
+// Each role is assigned a numeric value starting from 0 by default.
+enum UserRole {
+  Admin,
+  Manager,
+  Employee,
 }
 
-Object.values(ServerResponseStatus).forEach((value) => {
-  if (typeof value === 'number') {
-    console.log(value);
-  }
+type User = {
+  id: number;
+  name: string;
+  // Specify the user's role using the UserRole enum to ensure only valid predefined roles are allowed.
+  role: UserRole;
+  contact: [string, string];
+};
+
+function createUser(user: User): User {
+  return user;
+}
+
+const user: User = createUser({
+  id: 1,
+  name: 'john doe',
+  role: UserRole.Admin,
+  contact: ['john@gmail.com', '123-456-789'],
 });
 
-console.log(ServerResponseStatus);
-
-interface ServerResponse {
-  result: ServerResponseStatus;
-  data: string[];
-}
-
-function getServerResponse(): ServerResponse {
-  return {
-    result: ServerResponseStatus.Success,
-    data: ['first item', 'second item'],
-  };
-}
-
-const response: ServerResponse = getServerResponse();
-
-console.log(response);
+console.log(user);
