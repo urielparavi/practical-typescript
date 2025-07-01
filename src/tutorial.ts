f2c488941423954457ff0ca9ed9d08841efb90f2
Your Name <urielparavi75@gmail.com>
2025-07-01 18:32:59 +0300

    57. Type Guards - Truthy and Falsy

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,19 +1,13 @@
-type Dog = { type: 'dog'; name: string; bark: () => void };
-type Cat = { type: 'cat'; name: string; meow: () => void };
-type Animal = Dog | Cat;
-
-// function makeSound(animal: Animal) {
-//   if (animal.type === 'dog') {
-//     animal.bark();
-//   } else {
-//     animal.meow();
-//   }
-// }
-
-function makeSound(animal: Animal) {
-  if ('bark' in animal) {
-    animal.bark();
+function printLength(str: string | null | undefined) {
+  if (str) {
+    console.log(str.length);
   } else {
-    animal.meow();
+    console.log('no string provided');
   }
 }
+
+printLength('hello');
+printLength('');
+printLength(null);
+printLength();
+printLength(undefined);
