3b63838d8c6e3707724f4d4fababc948d3f1c560
Your Name <urielparavi75@gmail.com>
2025-07-02 00:28:35 +0300

    66. Generics - Multiple Types

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,22 +1,7 @@
-// let arrayStrings = createArray<string>(3, 'hello'); // ["hello", "hello", "hello"]
-// let arrayNumbers = createArray<number>(4, 100); // [100, 100, 100, 100]
-
-function generateStringArray(length: number, value: string): string[] {
-  let result: string[] = [];
-  result = Array(length).fill(value);
-  return result;
+function pair<T, U>(param1: T, param2: U): [T, U] {
+  return [param1, param2];
 }
 
-// console.log(generateStringArray(6, 'hello'));
+let result = pair<number, string>(123, 'hello');
 
-function createArray<T>(length: number, value: T): Array<T> {
-  let result: T[] = [];
-  result = Array(length).fill(value);
-  return result;
-}
-
-let arrayStrings = createArray<string>(10, 'hello');
-let arrayNumbers = createArray<number>(15, 100);
-
-console.log(arrayStrings);
-console.log(arrayNumbers);
+console.log(result);
