6bce8cdb89e2fc6648c064da65175898ea25b4d7
Your Name <urielparavi75@gmail.com>
2025-07-01 17:29:41 +0300

    Play 56. Type Guards - Equality and in

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,19 +1,19 @@
-type ValueType = string | number | boolean;
+type Dog = { type: 'dog'; name: string; bark: () => void };
+type Cat = { type: 'cat'; name: string; meow: () => void };
+type Animal = Dog | Cat;
 
-let value: ValueType;
-const random = Math.random();
-value = random < 0.33 ? 'Hello' : random < 0.66 ? 123.456 : true;
+// function makeSound(animal: Animal) {
+//   if (animal.type === 'dog') {
+//     animal.bark();
+//   } else {
+//     animal.meow();
+//   }
+// }
 
-function checkValue(value: ValueType): void {
-  if (typeof value === 'string') {
-    console.log(value.toLowerCase());
-    return;
+function makeSound(animal: Animal) {
+  if ('bark' in animal) {
+    animal.bark();
+  } else {
+    animal.meow();
   }
-  if (typeof value === 'number') {
-    console.log(value.toFixed(2));
-    return;
-  }
-  console.log(`boolean: ${value}`);
 }
-
-checkValue(value);
