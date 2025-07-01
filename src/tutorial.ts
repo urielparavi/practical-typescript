bdf69c2058d969d03d4864ca28b3eed9d2e75cdc
Your Name <urielparavi75@gmail.com>
2025-07-02 02:32:49 +0300

    68. Generics - Type Constraints Second Example

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,18 +1,37 @@
-function pair<T, U>(param1: T, param2: U): [T, U] {
-  return [param1, param2];
+type Car = {
+  brand: string;
+  model: string;
+};
+
+const car: Car = {
+  brand: 'ford',
+  model: 'mustang',
+};
+
+type Product = {
+  name: string;
+  price: number;
+};
+
+const product: Product = {
+  name: 'shoes',
+  price: 1.99,
+};
+
+type Student = {
+  name: string;
+  age: number;
+};
+
+const student: Student = {
+  name: 'peter',
+  age: 20,
+};
+
+function printName<T extends { name: string }>(input: T): void {
+  console.log(input.name);
 }
 
-let result = pair(123, 'Hello');
-
-//  const [name,setName] = useState('')
-//  const [products,setProducts] = useState<Product[]>([])
-
-// type constraint on the generic type T, generic type can be either a number or a string.
-
-function processValue<T extends string | number>(value: T): T {
-  console.log(value);
-  return value;
-}
-
-processValue('hello');
-processValue(12);
+printName(student);
+printName(product);
+// printName(car);
