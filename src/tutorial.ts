381110d684c9dd6e57c5b4bd6cebd1f9239d7187
Your Name <urielparavi75@gmail.com>
2025-07-01 19:19:18 +0300

    58. Type Guards - Instanceof

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,13 +1,23 @@
-function printLength(str: string | null | undefined) {
-  if (str) {
-    console.log(str.length);
+try {
+  throw new Error('This is an error');
+  // throw 'error by us';
+} catch (error) {
+  if (error instanceof Error) {
+    console.log(`Caught an Error object: ${error.message}`);
   } else {
-    console.log('no string provided');
+    console.log('unknown error...');
   }
 }
 
-printLength('hello');
-printLength('');
-printLength(null);
-printLength();
-printLength(undefined);
+function checkInput(input: Date | string): string {
+  if (input instanceof Date) {
+    return input.getFullYear().toString();
+  }
+  return input;
+}
+
+const dateYear = checkInput(new Date());
+const randomYearInString = checkInput('2022-05-05');
+
+console.log(dateYear);
+console.log(randomYearInString);
