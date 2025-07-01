80c36d39db81e6649faec0bfb53e14f60ad95aee
Your Name <urielparavi75@gmail.com>
2025-07-01 04:17:53 +0300

    51. Type - Never

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,28 +1,42 @@
-let unknownValue: unknown;
+let someValue: never = 0;
 
-unknownValue = 'hello';
-unknownValue = [1, 2, 3];
-unknownValue = 42.33455;
+type Theme = 'light' | 'dark';
 
-if (typeof unknownValue === 'number') {
-  unknownValue.toFixed(2);
+function checkTheme(theme: Theme): void {
+  if (theme === 'light') {
+    console.log('light theme');
+    return;
+  }
+  if (theme === 'dark') {
+    console.log('dark theme');
+    return;
+  }
+  theme;
 }
 
-function runSomeCode() {
-  const random = Math.random();
-  if (random < 0.5) {
-    throw new Error('there was error...');
-  } else {
-    throw 'some error';
+enum Color {
+  Red,
+  Blue,
+  // Green,
+}
+
+function getColorName(color: Color) {
+  switch (color) {
+    case Color.Red:
+      return 'Red';
+    case Color.Blue:
+      return 'Blue';
+    // case Color.Green:
+    //   return 'Green';
+    default:
+      // at build time
+      let unexpectedColor: never = color;
+
+      // at runtime
+      throw new Error(`Unexpected color value: ${color}`);
   }
 }
 
-try {
-  runSomeCode();
-} catch (error) {
-  if (error instanceof Error) {
-    console.log(error.message);
-  } else {
-    console.log(error);
-  }
-}
+console.log(getColorName(Color.Red));
+console.log(getColorName(Color.Blue));
+console.log(getColorName(Color.Green));
