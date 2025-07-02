50d208c1d385a40008991eaff647f1e80e1ea156
Your Name <urielparavi75@gmail.com>
2025-07-03 01:10:45 +0300

    87. Tasks Project - Event Gotcha

--- src/tasks.ts
+++ src/tasks.ts
@@ -10,7 +10,12 @@ type Task = {
 
 const task: Task[] = [];
 
-taskForm?.addEventListener('submit', (event) => {
+// Since this is a named function used as an event handler,
+// we must explicitly annotate the type of the 'event' parameter.
+// TypeScript cannot infer it automatically in this case,
+// unlike with inline functions passed directly to addEventListener,
+// where the event type is inferred from the context.
+function createTask(event: SubmitEvent) {
   event.preventDefault();
   const taskDescription = formInput?.value;
   if (taskDescription) {
@@ -20,4 +25,18 @@ taskForm?.addEventListener('submit', (event) => {
     return;
   }
   alert('Please enter a task description');
-});
+}
+
+taskForm?.addEventListener('submit', createTask);
+
+// taskForm?.addEventListener('submit', (event) => {
+//   event.preventDefault();
+//   const taskDescription = formInput?.value;
+//   if (taskDescription) {
+//     console.log(taskDescription);
+
+//     formInput.value = '';
+//     return;
+//   }
+//   alert('Please enter a task description');
+// });
