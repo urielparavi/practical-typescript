3c27fc43ed23d44441c1aa549d3fa973cf71a366
Your Name <urielparavi75@gmail.com>
2025-07-02 13:50:50 +0300

    73. Zod Library

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,13 +1,19 @@
+import { z } from 'zod';
+
 const url = 'https://www.course-api.com/react-tours-project';
 
-type Tour = {
-  id: string;
-  name: string;
-  info: string;
-  image: string;
-  price: string;
-  something: boolean;
-};
+const tourSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  info: z.string(),
+  image: z.string(),
+  price: z.string(),
+  // something: z.string(),
+});
+
+// Automatically infer the TypeScript type `Tour` from the `tourSchema` Zod schema.
+// This ensures that the validation schema and the TypeScript type always stay in sync.
+type Tour = z.infer<typeof tourSchema>;
 
 async function fetchData(url: string): Promise<Tour[]> {
   try {
@@ -15,10 +21,21 @@ async function fetchData(url: string): Promise<Tour[]> {
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    const data: Tour[] = await response.json();
-    console.log(data);
+    const rawData: Tour[] = await response.json();
 
-    return data;
+    // Validate that `rawData` is an array of objects matching the `tourSchema` structure.
+    // `safeParse` returns an object:
+    // - If validation passes → { success: true, data }
+    // - If validation fails  → { success: false, error }
+    // This avoids exceptions and gives you a safe way to handle invalid data.
+    const result = tourSchema.array().safeParse(rawData);
+
+    if (!result.success) {
+      throw new Error(`Invalid data: ${result.error}`);
+    }
+
+    console.log(result);
+    return result.data;
   } catch (error) {
     const errorMsg =
       error instanceof Error ? error.message : 'There was an error';
@@ -29,5 +46,5 @@ async function fetchData(url: string): Promise<Tour[]> {
 
 const tours = await fetchData(url);
 tours.map((tour) => {
-  console.log(tour);
+  console.log(tour.name);
 });
