841f7ded9bf89e8a812ed59d2da1d8cf90daf0ab
Your Name <urielparavi75@gmail.com>
2025-07-02 19:25:04 +0300

    81. Classes - Getters and Setters

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,16 +1,28 @@
 class Book {
   private checkedOut: boolean = false;
 
-  constructor(
-    readonly title: string,
-    public author: string,
-    private someValue: number
-  ) {}
-  public getSomeValue() {
-    return this.someValue;
+  constructor(readonly title: string, public author: string) {}
+
+  get info() {
+    return `${this.title} by ${this.author}`;
+  }
+
+  private set checkOut(checkedOut: boolean) {
+    this.checkedOut = checkedOut;
+  }
+  get checkOut() {
+    return this.checkedOut;
+  }
+
+  public get someInfo() {
+    this.checkOut = true;
+    return `${this.title} by ${this.author}`;
   }
 }
 
-const deepWork = new Book('deep work ', 'cal newport', 445);
-
-console.log(deepWork.getSomeValue());
+const deepWork = new Book('deep work ', 'cal newport');
+console.log(deepWork.info); // deep work  by cal newport
+// deepWork.checkOut = true;
+console.log(deepWork); // {checkedOut: false, title: 'deep work ', author: 'cal newport'}
+console.log(deepWork.someInfo); // deep work  by cal newport
+console.log(deepWork.checkOut); // true
