5d1e71333fd8e49d756c78c28ba7804c62f883ee
Your Name <urielparavi75@gmail.com>
2025-07-02 18:10:17 +0300

    80. Classes - Constructor Shortcut

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,27 +1,16 @@
 class Book {
-  public readonly title: string;
-  public author: string;
   private checkedOut: boolean = false;
 
-  constructor(title: string, author: string) {
-    this.title = title;
-    this.author = author;
-  }
-  public checkOut() {
-    this.checkedOut = this.toggleCheckedOutStatus();
-  }
-  public isCheckedOut() {
-    return this.checkedOut;
-  }
-  private toggleCheckedOutStatus() {
-    return !this.checkedOut;
+  constructor(
+    readonly title: string,
+    public author: string,
+    private someValue: number
+  ) {}
+  public getSomeValue() {
+    return this.someValue;
   }
 }
 
-const deepWork = new Book('deep work ', 'cal newport');
+const deepWork = new Book('deep work ', 'cal newport', 445);
 
-deepWork.checkOut(); // first call – from false → true
-deepWork.checkOut(); // second call – from true → false
-console.log(deepWork.isCheckedOut()); // prints false
-
-// console.log(deepWork); // {title: 'deep work ', author: 'cal newport'}
+console.log(deepWork.getSomeValue());
