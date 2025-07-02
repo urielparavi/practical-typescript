1052836ad727295d31ec066f2c444ac2fa0c2ec6
Your Name <urielparavi75@gmail.com>
2025-07-03 00:56:33 +0300

    86. Tasks Project - Submit Event

--- src/tasks.ts
+++ src/tasks.ts
@@ -9,3 +9,15 @@ type Task = {
 };
 
 const task: Task[] = [];
+
+taskForm?.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const taskDescription = formInput?.value;
+  if (taskDescription) {
+    console.log(taskDescription);
+
+    formInput.value = '';
+    return;
+  }
+  alert('Please enter a task description');
+});
