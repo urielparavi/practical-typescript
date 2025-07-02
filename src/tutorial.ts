3848d733aed3ad9ac0ec124ca6408fb56afed647
Your Name <urielparavi75@gmail.com>
2025-07-02 05:07:07 +0300

    70. Fetch Data - Basics

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,21 +1,24 @@
-// data is located in the data property
+const url = 'https://www.course-api.com/react-tours-project';
 
-const { data } = axios.get(someUrl);
+async function fetchData(url: string) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
 
-axios.get<{ name: string }[]>(someUrl);
-
-export class Axios {
-  get<T = any, R = AxiosResponse<T>, D = any>(
-    url: string,
-    config?: AxiosRequestConfig<D>
-  ): Promise<R>;
+    return data;
+  } catch (error) {
+    const errorMsg =
+      error instanceof Error ? error.message : 'There was an error';
+    console.log(errorMsg);
+    return [];
+  }
 }
 
-export interface AxiosResponse<T = any, D = any> {
-  data: T;
-  status: number;
-  statusText: string;
-  headers: RawAxiosResponseHeaders | AxiosResponseHeaders;
-  config: InternalAxiosRequestConfig<D>;
-  request?: any;
-}
+const tours = await fetchData(url);
+// tours.map((tour: any) => {
+//   console.log(tour.name);
+// });
