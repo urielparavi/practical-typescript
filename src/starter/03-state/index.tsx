b6aed91a86c12736e4fafde8f4a51abb1ec248b6
Your Name <urielparavi75@gmail.com>
2025-07-03 17:33:34 +0300

    98. State

--- src/starter/03-state/index.tsx
+++ src/starter/03-state/index.tsx
@@ -1,8 +1,23 @@
+import { useState } from 'react';
+
 function Component() {
+  const [text, setText] = useState('initial text');
+  const [number, setNumber] = useState(1);
+  const [list, setList] = useState<string[]>([]);
+
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>State</h2>
+      <h2 className="mb-1">React & Typescript</h2>
+      <button
+        className="btn btn-center"
+        onClick={() => {
+          // setText(1);
+          // setNumber(23);
+          setList(['hello', 'world']);
+        }}
+      >
+        click me
+      </button>
     </div>
   );
 }
