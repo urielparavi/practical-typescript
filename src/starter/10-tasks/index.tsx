db8e807d35167f8314d48518d098d1eb669b292d
Your Name <urielparavi75@gmail.com>
2025-07-05 04:56:53 +0300

    121. Tasks Project - Local Storage

--- src/starter/10-tasks/index.tsx
+++ src/starter/10-tasks/index.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { type Task } from './types';
 import Form from './Form';
 import List from './List';
 
+function loadTasks(): Task[] {
+  const storedTasks = localStorage.getItem('tasks');
+  return storedTasks ? JSON.parse(storedTasks) : [];
+}
+
+function updateStorage(tasks: Task[]): void {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
 function Component() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>(() => loadTasks());
 
   const addTask = (task: Task): void => {
     setTasks([...tasks, task]);
@@ -20,6 +29,10 @@ function Component() {
       })
     );
   };
+
+  useEffect(() => {
+    updateStorage(tasks);
+  }, [tasks]);
 
   return (
     <section>
